2c899e8871a93cabdf4ef52611413458fb7b200e
Aislan Feliciano <88755308+Aislan-Feliciano@users.noreply.github.com>
2022-04-05 13:52:08 -0300

    Adicionando arquivo inicial

---
+++ HTML-CSS-JS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velocidade que o carro atingiu foi ${vel}km/h`)
+if (vel > 70) { // condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija utilizando cinto de segurança.`)
